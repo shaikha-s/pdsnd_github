4b21c00ff0395b8c9e2952ed41f2de291556a114
root <root@localhost>
2024-11-04 11:39:19 +0000

    change the welcoming message

--- bikeshare.py
+++ bikeshare.py
@@ -15,7 +15,8 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print(print("""greetings!! welcome to the python bikeshare project, get ready to explore the bikeshare data
+"""))
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
      
     while True:
